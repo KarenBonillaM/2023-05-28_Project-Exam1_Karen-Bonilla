9b2b41ee331a84bc7b6d604d91704905d8811a74
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2023-05-10 20:27:37 +0200

    swexican blog

---
+++ js/posts.js
@@ -0,0 +1,63 @@
+//DISPLAYING THE BLOGS
+const apiBase = "https://wordpress.karenbonilla.se";
+const wordpressBase = "/wp-json/wp/v2";
+const blogsBase = "/posts?_embed";
+
+const fullBlogsURL = apiBase + wordpressBase + blogsBase;
+const blogsURL = "https://wordpress.karenbonilla.se/wp-json/wp/v2/posts?_embed";
+
+//GETTING DATA FROM API
+
+async function getBlogs() {
+  const response = await fetch (fullBlogsURL);
+
+  const posts = await response.json();
+
+  return posts;
+}
+
+function createPostHTML (post) {
+  const container = document.querySelector(".posts-container");
+
+  const postContainer = document.createElement("div");
+  postContainer.classList.add("post");
+  postContainer.classList.add("thumbnail-posts-page");
+
+  //IMAGES
+
+  const featuredImages = post._embedded["wp:featuredmedia"];
+
+  for(let i = 0; i < featuredImages.length; i++) {
+    const imgData = featuredImages[i];
+    const img = document.createElement("img");
+    img.classList.add("thumbnail")
+    img.src = imgData.source_url;
+    postContainer.append(img);
+  }
+
+  const title = document.createElement("a");
+  title.href = "/posts.html?id" + post.id
+  title.classList.add("post-title")
+  title.innerText = post.title.rendered;
+  postContainer.append(title);
+
+  container.append(postContainer);
+
+
+}
+
+function createPostsHTML (posts) {
+  for (let i = 0; i < posts.length; i++) {
+    const post = posts[i];
+    createPostHTML(post)
+  }
+}
+
+async function postSection () {
+  const posts = await getBlogs();
+  createPostsHTML(posts);
+}
+
+postSection()
+
+
