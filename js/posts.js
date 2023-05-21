f59b2ab5e9e31dbb0ba0998a95bd5824a1bf0025
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2023-05-21 18:06:11 +0200

    21 may

--- js/posts.js
+++ js/posts.js
@@ -7,6 +7,10 @@ const fullBlogsURL = apiBase + wordpressBase + blogsBase;
 const blogsURL = "https://wordpress.karenbonilla.se/wp-json/wp/v2/posts?_embed";
 
 
+
+const morePosts = "&page=2";
+const morePostsURL = apiBase + wordpressBase + blogsBase + morePosts;
+
 //GETTING DATA FROM API
 
 async function getBlogs() {
@@ -17,6 +21,18 @@ async function getBlogs() {
   return posts;
 }
 
+
+
+async function getMoreBlogs() {
+  const response = await fetch (morePostsURL);
+
+  const posts = await response.json();
+
+  return posts;
+}
+
+
+
 function createPostHTML (post) {
   const container = document.querySelector(".posts-container");
 
@@ -24,6 +40,20 @@ function createPostHTML (post) {
   postContainer.href = "posts/details.html?id=" + post.id;
   postContainer.classList.add("post");
   postContainer.classList.add("thumbnail-posts-page");
+
+  const btn = document.querySelector("#button-posts");
+  let currentPost = 10;
+
+  btn.addEventListener('click', function(){
+    for(let i = currentPost; i< currentPost+2; i++) {
+      if(postContainer[i]) {
+        postContainer[i].style.display = "block";
+      }
+    }  
+  })
+
+  currentPost += 2;
+  
 
   //IMAGES
 
@@ -47,12 +77,14 @@ function createPostHTML (post) {
 
 }
 
+
 function createPostsHTML (posts) {
   for (let i = 0; i < posts.length; i++) {
     const post = posts[i];
     createPostHTML(post)
   }
 }
+
 
 async function postSection () {
   const posts = await getBlogs();
@@ -61,7 +93,10 @@ async function postSection () {
 
 postSection()
 
+async function morePostSection () {
+  const posts = await getMoreBlogs();
+  createPostsHTML(posts);
+}
 
+morePostSection()
 
-/*By default Wordpress fetches 10 per page. I found it easiest to fetch page 2 so I only get the ones that are missing from the first fetch. My url looks like so /posts?per_page=10?page=2
-So I just added page 2 to the button activated fetch and ran that data through the renderPosts (or whatever you’ve named yours)*/
